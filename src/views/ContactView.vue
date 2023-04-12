1a9cbee6c9615a60575e37c3d846175432b185fa
wohligpankaj <pankaj.desale@wohlig.com>
2023-04-12 17:14:52 +0530

    init

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
-        contact
+      <v-parallax  height="600" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-parallax>
+      <div><h4>Please feel free to contact us regarding any of our products using the following details:</h4></div>
+      <div id="main">
+        <div><h3>Address:</h3>
+            <h5>Cara Ltd. <br>19 Avenue Road <br>Wansterd IP NY</h5>
+            <h3>Contact:</h3>
+            <h5></h5>
+        </div>
+        <div></div>
+      </div>
     </div>
+
+
 </template>
