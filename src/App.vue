2a472436866a8f2a701284151f0310120b32ba0a
TirthWohlig <tirthwohlig@gmail.com>
2023-04-12 16:33:04 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <v-app>
   <div>
-
+ <Header />
   </div>
   <router-view/>
   </v-app>
 </template>
 
 <script>
-
+import Header from "../src/components/Navbar.vue"
 export default {
   name: 'App',
 
+  components:{
+    Header,
+  },
   data: () => ({
   }),
 };
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,89 @@
 <template>
     <div>
-        
+        <section id="header">
+    <a href="#"><img src="../assets/logo.png" class="logo" /></a>
+    <div>
+      <ul id="navbar">
+        <li>
+          <a class="active" href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Shop</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a class="bag" href="#"><i class="fas fa-shopping-bag"></i></a>
+        </li>
+
+        <a href="#" id="close"><img class="close" src="/close.png" alt="" /></a>
+      </ul>
+    </div>
+    <div id="mobile">
+      <i class="fas fa-shopping-bag"></i>
+      <i id="bar" class="fas fa-outdent"></i>
+    </div>
+  </section>  
     </div>
 </template>
+
+<style scoped>
+#header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 80px;
+  background-color: #e3e6f5;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.06);
+  z-index: 999;
+  position: sticky;
+  top: 0;
+  left: 0;
+}
+#navbar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#navbar li {
+  list-style: none;
+  padding: 0 20px;
+  position: relative;
+}
+#navbar li a {
+  text-decoration: none;
+  font-size: 14px;
+  font-weight: 600;
+  color: #1a1a1a;
+  transition: 0.4s ease;
+}
+#navbar li a:hover {
+  color: #088178;
+}
+#navbar li a:hover,
+#navbar li a.active {
+  color: #088178;
+}
+#navbar li a.active::after, 
+#navbar li a:hover::after {
+  content: "";
+  width: 30%;
+  height: 2px;
+  background: #088178;
+  position: absolute;
+  bottom: -4px;
+  left: 20px;
+}
+#mobile {
+  display: none;
+  align-items: center;
+}
+#navbar.active {
+  right: 0px;
+}
+
+</style>
