5cfd8829df372e108a0dca2df35e31cbb89abbe7
TirthWohlig <tirthwohlig@gmail.com>
2023-04-13 11:54:01 +0530

    init

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,35 +1,36 @@
 <template>
-    <div>
-        <section id="header">
-    <a href="#"><img src="../assets/logo.png" class="logo" /></a>
-    <div>
-      <ul id="navbar">
-        <li>
-         <router-link to="/">Home</router-link>
-        </li>
-        <li>
-      <router-link to="/shop">Shop</router-link>
-        </li>
-        <li>
-        <router-link to="/about">About</router-link>
-        </li>
-        <li>
-          <router-link to="/contact">Contact</router-link>
-        </li>
-        <li>
-          <a class="bag" href="#"><i class="fas fa-shopping-bag"></i></a>
-        </li>
+  <div>
+    <section id="header">
+      <a href="#"><img src="../assets/logo.png" class="logo" /></a>
+      <div>
+        <ul id="navbar">
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/shop">Shop</router-link>
+          </li>
+          <li>
+            <router-link to="/about">About</router-link>
+          </li>
+          <li>
+            <router-link to="/contact">Contact</router-link>
+          </li>
+          <li>
+            <a class="bag" href="#"><i class="fas fa-shopping-bag"></i></a>
+          </li>
 
-        <a href="#" id="close"><img class="close" src="/close.png" alt="" /></a>
-      </ul>
-    </div>
-    <div id="mobile">
-      <i class="fas fa-shopping-bag"></i>
-      <i id="bar" class="fas fa-outdent"></i>
-    </div>
-        </section>  
-
-    </div>
+          <a href="#" id="close" @click="close()"
+            ><img height="30px" class="close" src="../assets/close.png" alt=""
+          /></a>
+        </ul>
+      </div>
+      <div id="mobile">
+        <i class="fas fa-shopping-bag"></i>
+        <i id="bar" @click="bar()" class="fas fa-outdent"></i>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style scoped>
@@ -69,7 +70,7 @@
 #navbar li a.active {
   color: #088178;
 }
-#navbar li a.active::after, 
+#navbar li a.active::after,
 #navbar li a:hover::after {
   content: "";
   width: 30%;
@@ -86,5 +87,77 @@
 #navbar.active {
   right: 0px;
 }
+@media (max-width: 799px) {
+  #navbar {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    position: fixed;
+    top: 0;
+    right: -300px;
+    height: 100vh;
+    width: 300px;
+    background-color: #e3e6f3;
+    box-shadow: 0 40px 60px rgba(0, 0, 0, 0.1);
+    padding: 80px 0 0 10px;
+    transition: 0.4s;
+  }
+  #navbar li {
+    margin-bottom: 25px;
+  }
+  #mobile {
+    display: flex;
+    align-items: center;
+  }
+  #mobile i {
+    color: #1a1a1a;
+    font-size: 24px;
+    padding-left: 20px;
+  }
+  #close {
+    position: absolute;
+    top: 30px;
+    left: 30px;
+    color: #1a1a1a;
+    font-size: 24px;
+  }
+  .bag {
+    display: none;
+  }
+}
 
+@media (min-width: 799px) {
+  .close {
+    display: none;
+  }
+}
+@media (max-width: 600px) {
+  #header {
+    padding: 10px 30px;
+  }
+
+  h2 {
+    font-size: 32px;
+  }
+  h1 {
+    font-size: 38px;
+  }
+}
 </style>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    close() {
+      navbar.classList.remove("active");
+    },
+    bar() {
+      navbar.classList.toggle("active");
+    },
+  },
+};
+</script>
