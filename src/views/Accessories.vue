9ff7749af8307d738d96e9a5b32a99b897689f03
TirthWohlig <tirthwohlig@gmail.com>
2023-04-14 16:12:54 +0530

    init

---
+++ src/views/Accessories.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Header />
+    <div class="container1">
+      <h1 class="centered">
+        Sorry, looks like we are currently unable to find anything that matches
+        your criteria
+      </h1>
+    </div>
+    <br />
+    <Footer />
+  </div>
+</template>
+ 
+<style scoped>
+h1 {
+  text-align: center;
+}
+
+.container1 {
+  top: 50%;
+}
+</style>
+  
+  <script>
+import Header from "../components/Navbar.vue";
+import Footer from "../components/Footer.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+  
