e7d61d7ee2152437a33e76adf5693cec55ab761f
wohligpankaj <pankaj.desale@wohlig.com>
2023-04-14 17:49:59 +0530

    ghfhfhg

--- src/views/Dresses.vue
+++ src/views/Dresses.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <Header />
+    <Header/>
     <v-row>
-      <v-col cols="12" md="3" v-for="items in sari" :key="items">
+      <v-col cols="12" md="3" v-for="dress in dresses" :key="dress">
         <v-card class="mx-auto my-12" max-width="400">
-          <v-img height="600px" :src="items.image"></v-img>
-          <v-card-title>Designed By:  {{ items.title }}</v-card-title>
+          <v-img height="600px" :src="dress.image"></v-img>
+          <v-card-title>Designed By:  {{ dress.title }}</v-card-title>
           <v-card-text>
             <v-row align="center" class="mx-0">
               <v-rating
-                :value="items.rating"
+                :value="dress.rating"
                 color="amber"
                 dense
                 half-increments
@@ -18,16 +18,16 @@
               ></v-rating>
 
               <div class="grey--text mt-3">
-                {{ items.rating }} ({{ items.volume }})
+                {{ dress.rating }} ({{ dress.volume }})
               </div>
             </v-row>
 
             <div class="my-4 text-subtitle-1">
-              ₹{{ items.price }}  
+              ₹{{ dress.price }}  
             </div>
 
             <div>
-              {{ items.description }}
+              {{ dress.description }}
             </div>
           </v-card-text>
         </v-card>
@@ -62,13 +62,13 @@ import axios from "axios";
 export default {
   data() {
     return {
-      sari: "",
+      dresses: "",
     };
   },
   mounted() {
-    axios.get("http://localhost:3000/saris_sets").then((response) => {
-      this.sari = response.data;
-      console.log(this.sari);
+    axios.get("http://localhost:3000/dresses").then((response) => {
+      this.dresses = response.data;
+      console.log(this.dresses);
     });
   },
   components: {
